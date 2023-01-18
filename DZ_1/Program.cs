bf19f9a85e98f9e13290271b6793d088ab8a7dd9
«Alexandr» <>
2023-01-19 02:52:38 +0500

    Та самая домашка

---
+++ DZ_1/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+Console.Clear();
+int a = eeee("Введите число: ");
+int b = eeee("Введите степень: ");
+functstep(a, b);
+
+
+// Функция возведения в степень
+void functstep(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(a + " в степени " + b + " = " + result);
+}
+
+// Функция ввода
+int eeee(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
---
+++ DZ_3/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+ */
+Console.Clear();
+
+int [] mass = new int[8];
+for(int i = 0; i < mass.Length; i++)
+{
+    mass [i] = new Random().Next(999);
+} 
+Console.WriteLine($"Ваш массив {String.Join(", ", mass)}");
